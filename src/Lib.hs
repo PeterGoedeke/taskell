fe8e119d1e93a9e26e400633b849b5e72d4bad50
PeterGoedeke <peterbgoedeke@gmail.com>
2020-08-11 22:36:53 +1200

    Task type

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,2 +1,9 @@
 module Lib where
 
+data Task a = Task a | Category [Task a] deriving (Show)
+
+instance Functor Task where
+    fmap f (Task a) = Task (f a)
+    fmap f (Category a) = Category (map (\x -> fmap f x) a)
+
+test = Category [Task "h", Task "i"]
