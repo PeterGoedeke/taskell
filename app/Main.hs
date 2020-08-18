ee68f4ba7645b842fc6f7e4446bbf514f8b1f4e3
PeterGoedeke <peterbgoedeke@gmail.com>
2020-08-18 16:52:27 +1200

    Added a function which loops until the user has entered a valid int

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,26 @@
 module Main where
 
+import System.IO
+import Control.Monad (when)
+import Data.Char
+import Control.Exception
+import Text.Read
+
+import qualified Data.IntMap.Strict as Im
+
 import Lib
 
 main = undefined
+getInt :: IO (Int)
+getInt = do
+    value <- getLine
+    getInt' $ (readMaybe value :: Maybe Int)
+
+getInt' :: (Maybe Int) -> IO (Int)
+getInt' (Just a) = return a
+getInt' Nothing = do
+    putStrLn "Invalid input"
+    value <- getLine
+    let parsed = readMaybe value :: Maybe Int
+    getInt' parsed
+
